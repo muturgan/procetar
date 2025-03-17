84c74f55b7e522ade442f37f024c96e3752584f4
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2025-03-17 18:57:44 +0300

    make a sourcecode readable

--- index.mjs
+++ index.mjs
@@ -1,1 +1,17 @@
-export const procetar=s=>crypto.subtle.digest("SHA-1",new TextEncoder().encode(s)).then(t=>{let e=Array.from(new Uint8Array(t)),o=e.slice(0,3).map(t=>t.toString(16)).join(""),l="",r="",n="h1v1h-1z";for(let _=0;_<16;_++){let f=_%4,s=Math.floor(_/4),i=`M${f} ${s}${n}M${7-f} ${s}${n}M${f} ${7-s}${n}M${7-f} ${7-s}${n}`;e[_]<128?l+=i:r+=i}return`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><path fill="#${o}" d="${l}"/><path fill="#fff" d="${r}"/></svg>`});
+export const procetar = (str) => crypto.subtle.digest("SHA-1",new TextEncoder().encode(str))
+	.then((buf) => {
+		let u8s = Array.from(new Uint8Array(buf)),
+		d1 = "",
+		d2 = "",
+		rect = "h1v1h-1z";
+
+		for (let i = 0; i < 16; i++) {
+			let x = i % 4,
+			y = Math.floor(i / 4),
+			d = `M${ x } ${ y }${ rect }M${ 7 - x } ${ y }${ rect }M${ x } ${ 7 - y }${ rect }M${ 7 - x } ${ 7 - y }${ rect }`;
+			u8s[i] < 128 ? d1 += d : d2 += d;
+		}
+
+		return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><path fill="#${ u8s.slice(0, 3).map((n) => n.toString(16)).join("") }" d="${ d1 }"/><path fill="#fff" d="${ d2 }"/></svg>`;
+	}
+);

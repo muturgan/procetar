6d8ff334be66c19ca52c98669f08ba749321db73
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2025-03-18 08:57:35 +0300

    exec createObjectURL for you

--- index.mjs
+++ index.mjs
@@ -12,6 +12,11 @@ export const procetar = (str) => crypto.subtle.digest("SHA-1",new TextEncoder().
 			u8s[i] < 128 ? d1 += d : d2 += d;
 		}
 
-		return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><path fill="#${ u8s.slice(0, 3).map((n) => n.toString(16)).join("") }" d="${ d1 }"/><path fill="#fff" d="${ d2 }"/></svg>`;
+		return URL.createObjectURL(
+			new Blob(
+				[`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><path fill="#${ u8s.slice(0, 3).map((n) => n.toString(16)).join("") }" d="${ d1 }"/><path fill="#fff" d="${ d2 }"/></svg>`],
+				{type: "image/svg+xml"}
+			)
+		);
 	}
 );
